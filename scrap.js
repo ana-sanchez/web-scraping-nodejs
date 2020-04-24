ef9aa75ad00195d4274c3c085fe1e313743f4410
ana-sanchez <snchz.rojo@gmail.com>
2020-04-24 11:39:09 +0200

    add archive js

---
+++ scrap.js
@@ -0,0 +1,17 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+const fs = require("fs")
+
+let names = [
+  `Niels Bohr`,
+  `Marie Curie`,
+  `Albert Einstein`,
+  `Isaac Newton`,
+  `Galileo Galilei`,
+  `James Clerk Maxwell`,
+  `Michael Faraday`,
+  `Richard Feynman`,
+  `Ernest Rutherford`,
+  `Paul Dirac`
+
+]
